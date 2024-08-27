ef0b3d6ef219dc1b66a77d9b48c2e469a6f7d832
shuhaib <shuhaibchemban2004@gmail.com>
2024-08-27 22:34:41 +0530

    created a django repo

---
+++ django_startup/django_start.py
@@ -0,0 +1,13 @@
+"""
+for install django:-
+    py -m pip install Django
+
+for create new project :-
+    django-admin startproject project_name
+
+for create new app in project:-
+    py manage.py startapp app_name
+
+for running project :-
+    py manage.py runserver
+"""
---
+++ django_startup/env_setup.py
@@ -0,0 +1,12 @@
+"""
+
+Virtual Environment
+It is suggested to have a dedicated virtual environment for each Django project, and one way to manage a virtual environment is venv, which is included in Python.
+
+For Creating a new env:-
+    py -m venv myworld
+
+For Activate env:-
+    myworld\Scripts\activate.bat
+
+"""
