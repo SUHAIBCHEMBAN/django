5adf1a85eadc12a496c6c6a037b857245874990e
shuhaib <shuhaibchemban2004@gmail.com>
2024-09-03 22:19:12 +0530

    add new chapters

---
+++ MVC and MVT/diff.py
@@ -0,0 +1,9 @@
+"""
+Key Differences:
+View in MVC vs. MVT: In MVC, the View handles both displaying the data and user interaction. In Django's MVT, the View handles only business logic, while the Template handles the actual presentation.
+Controller in MVC vs. MVT: In Django, the Controller part is more abstract and is handled by the framework itself, which maps URLs to Views.
+
+
+Conclusion:
+Both MVC and MVT aim to separate concerns within a web application, making it easier to manage and scale. Django's MVT pattern is a specialized version tailored for web development in Python, streamlining many common tasks with built-in tools and features.
+"""
---
+++ MVC and MVT/mvc.py
@@ -0,0 +1,13 @@
+"""
+
+MVC (Model-View-Controller) Pattern
+Explanation:
+
+Model: Represents the data structure and business logic of the application. It interacts with the database, retrieves, and manipulates data.
+View: Represents the user interface and presentation layer. It displays the data from the model to the user and sends user commands to the controller.
+Controller: Acts as an intermediary between the Model and the View. It receives user input from the View, processes it (often updating the Model), and returns the output display to the View.
+Example:
+
+In a simple web application, the Model would handle the database queries, the View would render HTML templates, and the Controller would process user inputs like form submissions.
+
+"""
